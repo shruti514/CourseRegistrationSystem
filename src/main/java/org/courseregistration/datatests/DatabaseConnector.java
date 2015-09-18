9b0f3b40a3af44c4ce0f06bde28d44595edbe59a
shraddhakannav <shraddhakannav@gmail.com>
2015-09-18 14:57:23 -0700

    Database connection Test Commit to check

--- src/main/java/org/courseregistration/datatests/DatabaseConnector.java
+++ src/main/java/org/courseregistration/datatests/DatabaseConnector.java
@@ -1,15 +1,36 @@
 package org.courseregistration.datatests;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
-//This is a singleton class to return connection object
+/*
+ * This is a singleton class to return connection object
+ */
 public class DatabaseConnector {
-    private static Connection connection = null;
 
-    public static Connection getDBConnection(){
-        if(connection==null){
-           //establish connection
-        }
-        return connection;
-    }
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+	static final String DB_URL = "jdbc:mysql://localhost:3307/taxi_cab_system";
+
+	// Database credentials
+
+	static final String USER = "root";
+
+	static final String PASS = "root";
+
+	private static Connection connection = null;
+
+	public static Connection getDBConnection() {
+
+		if (connection == null) {
+			try {
+				Class.forName(JDBC_DRIVER);
+				System.out.println("Connecting to database...");
+				connection = DriverManager.getConnection(DB_URL, USER, PASS);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return connection;
+	}
 }
