6ae73f453106b6a07132ef9111f3755f40059a1b
shraddhakannav <shraddhakannav@gmail.com>
2015-09-18 16:48:45 -0700

    Database connection with accepting IPaddress, port and username, password for database server

--- src/main/java/org/courseregistration/datatests/DatabaseConnector.java
+++ src/main/java/org/courseregistration/datatests/DatabaseConnector.java
@@ -1,5 +1,7 @@
 package org.courseregistration.datatests;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -10,13 +12,18 @@ public class DatabaseConnector {
 
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 
-	static final String DB_URL = "jdbc:mysql://localhost:3307/taxi_cab_system";
+	static String IPADRESS = "localhost";
+
+	static String PORT = "3307";
+
+	static final String DB_URL = "jdbc:mysql://" + IPADRESS + ":" + PORT
+			+ "/courseregistartion";
 
 	// Database credentials
 
-	static final String USER = "root";
+	static String USER = "root";
 
-	static final String PASS = "root";
+	static String PASS = "root";
 
 	private static Connection connection = null;
 
@@ -32,5 +39,58 @@ public class DatabaseConnector {
 			}
 		}
 		return connection;
+	}
+
+	public static void getUserAndPWD() {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					System.in));
+			String thisLine = "";
+
+			System.out.println("Enter user to access database: ");
+			thisLine = br.readLine().trim();
+			if (thisLine != null)
+				USER = thisLine;
+
+			System.out.println("Enter password to access database: ");
+			thisLine = br.readLine().trim();
+			if (thisLine != null)
+				PASS = thisLine;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void getIPOfServer() {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					System.in));
+
+			System.out.println("Enter IP address of Server: ");
+			String line = "";
+			if ((line = br.readLine()) != null)
+				IPADRESS = line.trim();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+
+	public static void getServerPort() {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					System.in));
+
+			System.out.println("Enter IP address of Server: ");
+			String line = "";
+			if ((line = br.readLine()) != null)
+				PORT = line.trim();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
 	}
 }
