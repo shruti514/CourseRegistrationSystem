e36f03809d58dc3a139c825561739af7d99c8be8
shraddhakannav <shraddhakannav@gmail.com>
2015-09-27 16:16:01 -0700

    DB Params in DB Connector

--- src/main/java/org/courseregistration/dbtests/DatabaseConnector.java
+++ src/main/java/org/courseregistration/dbtests/DatabaseConnector.java
@@ -13,6 +13,7 @@ public class DatabaseConnector {
 	// static String DBNAME = "student_registration_database";
 
 	static String PORT = "3306";
+	// static String PORT = "3307";
 
 	static final String DB_URL = "jdbc:mysql://" + IPADRESS + ":" + PORT + "/"
 			+ DBNAME;
@@ -22,5 +23,6 @@ public class DatabaseConnector {
 	static String USER = "root";
 
 	static String PASS = "";
+	// static String PASS = "root";
 
 }
