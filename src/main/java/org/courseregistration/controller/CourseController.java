4610243ca630c1e5c8208b7fd3288edf3d3e738a
Neha-Ramachandra <nralka90@gmail.com>
2015-10-25 16:32:02 -0700

    neha Course service

--- src/main/java/org/courseregistration/controller/CourseController.java
+++ src/main/java/org/courseregistration/controller/CourseController.java
@@ -1,5 +1,6 @@
 package org.courseregistration.controller;
 
+<<<<<<< aa862c766f205259a243c0ada848db50b83b6437
 
 import org.courseregistration.model.Course;
 import org.courseregistration.service.CourseService;
@@ -7,11 +8,20 @@ import org.hibernate.annotations.GeneratorType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+=======
+import org.courseregistration.model.Course;
+import org.courseregistration.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.ws.rs.DELETE;
+>>>>>>> neha Course service
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+<<<<<<< aa862c766f205259a243c0ada848db50b83b6437
 import java.awt.*;
 
 @Service
@@ -39,4 +49,49 @@ public class CourseController {
     public Course findCourseById(@PathParam("id") Long id) {
        return courseService.findById(id);
     }
+=======
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Service
+@Path("courses")
+public class CourseController 
+{
+	 @Autowired
+	    private CourseService courseService;
+
+	    /**
+	     * Get details of a specific course
+	     *
+	     * @param id course identifier of the required course
+	     *
+	     *
+	     * @response.representation.200.doc Details of course
+	     * @response.representation.200.mediaType application/json
+	     *
+	     * @response.representation.404.doc Requested course with id not found
+	     *
+	     * @return details of a course
+	     */
+	    @GET
+	    @Path("{id}")
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public Course findCourseById(@PathParam("id") Long id) {
+	       return courseService.findById(id);
+	    }
+	    
+	    @GET
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public Response findCourses() {
+	        List<Course> allCourses = courseService.findAllCourses();
+	        return Response.ok().entity(allCourses).build();
+	    }
+	    
+	    @DELETE
+	    @Path("{id}")
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public void deleteCourseById(@PathParam("id") Long id) {
+	       courseService.deleteById(id); 
+	    }
+>>>>>>> neha Course service
 }
--- src/main/java/org/courseregistration/service/CourseService.java
+++ src/main/java/org/courseregistration/service/CourseService.java
@@ -1,9 +1,13 @@
 package org.courseregistration.service;
 
+import java.util.List;
+
 import org.courseregistration.dao.CourseDAO;
 import org.courseregistration.model.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jersey.repackaged.com.google.common.collect.Lists;
 
 /**
  * Created by cpunekar on 25-Oct-15.
@@ -12,10 +16,45 @@ import org.springframework.stereotype.Service;
 @Service
 public class CourseService {
     @Autowired
-    private CourseDAO courseDAO;
+    private CourseDAO courseDao;
 
-    public Course findById(Long id) {
-       Course toReturn = courseDAO.findById(id);
+
+	/**
+     * Find list of Courses
+     *
+     * @return list of all Courses
+     */
+    //@Override
+	public List<Course> findAllCourses()
+			{
+				List<Course> courses = courseDao.findAll();
+				List<Course> toReturn = Lists.newArrayList();
+				for(Course course: courses)
+				{
+					toReturn.add(course);
+				}
+				return toReturn;
+			}
+	/**
+     * Find a course by course id
+     *
+     * @return the course details fetched from the database of the corresponding course id
+     */
+    //@Override
+	public Course findById(Long id) 
+	{
+        Course toReturn = courseDao.findById(id);
         return toReturn;
+	}
+	
+	/**
+     * Delete a course given the course id
+     *
+     * @return the status 
+     */
+    //@Override
+	public void deleteById(Long id) 
+    {
+        courseDao.delete(id);
     }
 }
