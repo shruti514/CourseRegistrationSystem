7234c2fdbed3a4fbdcd94708a570c00df44269a3
Shital Kadam <kadam.s.d.2009@gmail.com>
2015-10-25 16:25:30 -0700

    Added price for course

--- src/main/java/org/courseregistration/controller/ProfessorController.java
+++ src/main/java/org/courseregistration/controller/ProfessorController.java
@@ -1,16 +1,22 @@
 package org.courseregistration.controller;
 
+import org.courseregistration.model.Professor;
+import org.courseregistration.model.Student;
+import org.courseregistration.service.ProfessorService;
 import org.jvnet.hk2.annotations.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Service
 @Path("professor")
 
 public class ProfessorController {
     @Autowired
-    private ProfessorService professoersrevice;
+    private ProfessorService professorsrevice;
 
     /**
      * Get details of a specific professor
@@ -24,4 +30,25 @@ public class ProfessorController {
      *
      * @return details of a professor
      */
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Professor findProfessorById(@PathParam("id") Long id) {
+        return professorsrevice.findById(id) ;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findProfessors(){
+        List<Professor> allProfessors = professorsrevice.findAllProfessors();
+        return Response.ok().entity(allProfessors).build();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addProfessor(Professor p){
+         professorsrevice.addProfessoor(p);
+        return Response.ok().entity(p).build() ;
+    }
+
 }
