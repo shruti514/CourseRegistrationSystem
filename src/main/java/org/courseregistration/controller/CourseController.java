0f52d26467ba12bc96ffbea26290da71e471961a
shraddhakannav <shraddhakannav@gmail.com>
2015-10-25 18:04:56 -0700

    Course Controller and service updated Section Controller Added

--- src/main/java/org/courseregistration/controller/CourseController.java
+++ src/main/java/org/courseregistration/controller/CourseController.java
@@ -1,97 +1,66 @@
 package org.courseregistration.controller;
 
-<<<<<<< aa862c766f205259a243c0ada848db50b83b6437
+import java.util.List;
 
-import org.courseregistration.model.Course;
-import org.courseregistration.service.CourseService;
-import org.hibernate.annotations.GeneratorType;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-=======
-import org.courseregistration.model.Course;
-import org.courseregistration.service.CourseService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
->>>>>>> neha Course service
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-<<<<<<< aa862c766f205259a243c0ada848db50b83b6437
-import java.awt.*;
+import javax.ws.rs.core.Response;
 
-@Service
-@Path("courses")
+import org.courseregistration.model.Course;
+import org.courseregistration.service.CourseService;
 
 public class CourseController {
-    @Autowired
-     private CourseService courseService;
-    /**
-     * Get details of a course
-     *
-     * @param id course identifier of the required course
-     *
-     * @response.representation.200.doc Details of course
-     * @response.representation.200.mediaType application/json
-     *
-     * @response.representation.404.doc Requested course with id not found
-     *
-     * @return details of the course
-     */
-    @GET
-    @Path("{id}")
-    @Produces(MediaType.APPLICATION_JSON)
 
-    public Course findCourseById(@PathParam("id") Long id) {
-       return courseService.findById(id);
-    }
-=======
-import javax.ws.rs.core.Response;
-import java.util.List;
+	private CourseService courseService;
 
-@Service
-@Path("courses")
-public class CourseController 
-{
-	 @Autowired
-	    private CourseService courseService;
+	/**
+	 * Get details of a specific course
+	 *
+	 * @param id
+	 *            course identifier of the required course
+	 *
+	 *
+	 * @response.representation.200.doc Details of course
+	 * @response.representation.200.mediaType application/json
+	 *
+	 * @response.representation.404.doc Requested course with id not found
+	 *
+	 * @return details of a course
+	 */
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Course findCourseById(@PathParam("id") Long id) {
+		return courseService.findById(id);
+	}
 
-	    /**
-	     * Get details of a specific course
-	     *
-	     * @param id course identifier of the required course
-	     *
-	     *
-	     * @response.representation.200.doc Details of course
-	     * @response.representation.200.mediaType application/json
-	     *
-	     * @response.representation.404.doc Requested course with id not found
-	     *
-	     * @return details of a course
-	     */
-	    @GET
-	    @Path("{id}")
-	    @Produces(MediaType.APPLICATION_JSON)
-	    public Course findCourseById(@PathParam("id") Long id) {
-	       return courseService.findById(id);
-	    }
-	    
-	    @GET
-	    @Produces(MediaType.APPLICATION_JSON)
-	    public Response findCourses() {
-	        List<Course> allCourses = courseService.findAllCourses();
-	        return Response.ok().entity(allCourses).build();
-	    }
-	    
-	    @DELETE
-	    @Path("{id}")
-	    @Produces(MediaType.APPLICATION_JSON)
-	    public void deleteCourseById(@PathParam("id") Long id) {
-	       courseService.deleteById(id); 
-	    }
->>>>>>> neha Course service
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response findCourses() {
+		List<Course> allCourses = courseService.findAllCourses();
+		return Response.ok().entity(allCourses).build();
+	}
+
+	@DELETE
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteCourseById(@PathParam("id") Long id) {
+		courseService.deleteById(id);
+	}
+
+	@POST
+	@Path("/post")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createTrackInJSON(Course course) {
+
+		String result = "Course saved : " + course;
+		courseService.save(course);
+		return Response.status(201).entity(result).build();
+	}
 }
--- src/main/java/org/courseregistration/controller/SectionController.java
+++ src/main/java/org/courseregistration/controller/SectionController.java
@@ -1,7 +1,99 @@
 package org.courseregistration.controller;
 
+import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.courseregistration.model.Section;
+import org.courseregistration.service.SectionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Path("sections")
 public class SectionController {
+
+	@Autowired
+	private SectionService sectionService;
+
+	/**
+	 * Get details of a specific student
+	 *
+	 * @param id
+	 *            student identifier of the required student
+	 *
+	 * @response.representation.200.doc Details of student
+	 * @response.representation.200.mediaType application/json
+	 *
+	 * @response.representation.404.doc Requested student with id not found
+	 *
+	 * @return details of a student
+	 */
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Section findStudentById(@PathParam("id") Long id) {
+		return sectionService.findById(id);
+	}
+
+	@GET
+	@Path("/name:{name}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response findStudentById(@PathParam("name") String name) {
+		List<Section> allStudents = sectionService.findByName(name);
+		return Response.ok(200).entity(allStudents).build();
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response findStudents() {
+		List<Section> allStudents = sectionService.findAllSections();
+		return Response.ok(200).entity(allStudents).build();
+	}
+
+	@POST
+	@Path("/post")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createTrackInJSON(Section section) {
+
+		String result = "Section saved : " + section;
+		sectionService.save(section);
+		return Response.status(201).entity(result).build();
+
+	}
+
+	@PUT
+	@Path("/{param}")
+	public Response putMsg(@PathParam("param") Section section) {
+		String output = "PUT: Jersey say : " + section;
+
+		sectionService.update(section);
+		return Response.status(200).entity(output).build();
+	}
+
+	@DELETE
+	@Path("/{param}")
+	public Response deleteMsg(@PathParam("param") Section section) {
+		String output = "DELETE:Jersey say : " + section;
+		sectionService.delete(section);
+		return Response.status(200).entity(output).build();
+	}
+
+	@HEAD
+	@Path("/{param}")
+	public Response headMsg(@PathParam("param") String msg) {
+		String output = "HEAD:Jersey say : " + msg;
+		return Response.status(200).entity(output).build();
+	}
 
 }
