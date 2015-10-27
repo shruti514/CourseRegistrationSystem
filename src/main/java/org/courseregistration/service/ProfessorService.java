01f76474f5096f1a54e8bbfb9fac7f1f17a1e95c
Shital Kadam <kadam.s.d.2009@gmail.com>
2015-10-27 11:50:28 -0700

    Professor attributes update

--- src/main/java/org/courseregistration/service/ProfessorService.java
+++ src/main/java/org/courseregistration/service/ProfessorService.java
@@ -4,9 +4,11 @@ import org.courseregistration.dao.SearchCriteria;
 import org.courseregistration.model.Professor;
 import org.courseregistration.dao.ProfessorDAO;
 import org.courseregistration.model.Section;
+import org.courseregistration.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -38,10 +40,19 @@ public class ProfessorService {
         professorDAO.save(p);
     }
 
+    public void addProfessors(List<Professor> p)  {
+        professorDAO.save(p);
+    }
+
     public void deleteProfessorById(Long id){
         professorDAO.delete(id);
     }
 
+    public void deleteAllProfessors(Collection<Long> ids){
+        professorDAO.delete(ids);
+    }
+
+    /*
     public void updateProfessorPassword(Long id,String password){
         Professor professor = professorDAO.findById(id) ;
         professor.setHashedPassword(password);
@@ -58,6 +69,69 @@ public class ProfessorService {
         Professor professor = professorDAO.findById(id);
         professor.setFacultyType(facultyType);
         professorDAO.save(professor);
+    }  */
+
+    public void updateProfessor(Long id ,Professor p){
+        Professor currentProfessor = professorDAO.findById(id);
+
+        if(p.getHashedPassword() != null){
+            currentProfessor.setHashedPassword(p.getHashedPassword());
+        }
+        if(p.getPhoneNumber() != null){
+            currentProfessor.setPhoneNumber(p.getPhoneNumber());
+        }
+        if(p.getFacultyType() != null){
+            currentProfessor.setFacultyType(p.getFacultyType());
+        }
+        if(p.getOfficeHoursFromTime() != null){
+            currentProfessor.setOfficeHoursFromTime(p.getOfficeHoursFromTime());
+        }
+        if (p.getOfficeHoursToTime() != null)  {
+            currentProfessor.setOfficeHoursToTime(p.getOfficeHoursToTime());
+        }
+        if(p.getDateOfBirth() != null) {
+            currentProfessor.setDateOfBirth(p.getDateOfBirth());
+        }
+        if(p.getYearsOfExperience()!=null){
+            currentProfessor.setYearsOfExperience(p.getYearsOfExperience());
+        }
+        if(p.getAddress() != null){
+            currentProfessor.setAddress(p.getAddress());
+        }
+        if(p.getCollegeId() !=null){
+            currentProfessor.setCollegeId(p.getCollegeId());
+        }
+        if (p.getFirstName() !=null){
+            currentProfessor.setFirstName(p.getFirstName());
+        }
+        if(p.getMiddleName() !=null){
+            currentProfessor.setMiddleName(p.getMiddleName());
+        }
+        if(p.getLastName() != null){
+            currentProfessor.setLastName(p.getLastName());
+        }
+        professorDAO.update(currentProfessor)  ;
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
