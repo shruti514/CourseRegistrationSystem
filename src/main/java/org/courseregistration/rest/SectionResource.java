bf122cb3eea3bfde82f0ef1fc323a2da7d40fe6e
shraddhakannav <shraddhakannav@gmail.com>
2015-11-16 13:21:50 -0800

    Section resource wrapper

--- src/main/java/org/courseregistration/rest/SectionResource.java
+++ src/main/java/org/courseregistration/rest/SectionResource.java
@@ -1,5 +1,7 @@
 package org.courseregistration.rest;
 
+import static org.courseregistration.rest.ResponseHelper.getCacheControl;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -8,27 +10,42 @@ import java.util.concurrent.TimeUnit;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.*;
-import javax.ws.rs.core.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import org.courseregistration.dao.SearchCriteria;
 import org.courseregistration.exception.ApplicationException;
 import org.courseregistration.hateoas.SectionResourceWrapper;
-import org.courseregistration.hateoas.StudentResourceWrapper;
 import org.courseregistration.model.Section;
 import org.courseregistration.rest.writters.SectionAssembler;
 import org.courseregistration.service.SectionService;
-import org.courseregistration.webconfig.CacheControlAnnotation;
 import org.courseregistration.webconfig.CacheControlAnnotation.CacheMaxAge;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.*;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resources;
 import org.springframework.hateoas.jaxrs.JaxRsLinkBuilder;
 import org.springframework.stereotype.Component;
 
-import static org.courseregistration.rest.ResponseHelper.getCacheControl;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 @Component
 @Path("sections")
@@ -44,49 +61,46 @@ public class SectionResource {
 	/**
 	 * Get details of a specific Section
 	 *
-	 * @param id section identifier
+	 * @param id
+	 *            section identifier
 	 * @return details of a Section
 	 */
 	@GET
 	@Path("/{id}")
-    @CacheMaxAge(time = 10, unit = TimeUnit.MINUTES, isPrivate=false,noStore=false)
+	@CacheMaxAge(time = 10, unit = TimeUnit.MINUTES, isPrivate = false, noStore = false)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response findSectionById(@PathParam("id") Long id,
-                                    @Context Request request)
-			throws ApplicationException {
+			@Context Request request) throws ApplicationException {
+
 		Section section = sectionService.findById(id);
-		if (section != null) {
-			Resource<Section> resource = new Resource<>(section);
-			Link selfRel = entityLinks.linkToSingleResource(Section.class,
-					section.getId()).withSelfRel();
-			resource.add(selfRel);
+		SectionAssembler sectionAssembler = new SectionAssembler();
+		SectionResourceWrapper resources = sectionAssembler.toResource(section);
 
-            int hashCode = section.hashCode();
+		int hashCode = section.hashCode();
 
-            EntityTag tag = new EntityTag(Integer.toString(hashCode));
-            Date lastUpdated = section.getUpdatedAt();
+		EntityTag tag = new EntityTag(Integer.toString(hashCode));
+		Date lastUpdatedAt = section.getUpdatedAt();
 
+		Response.ResponseBuilder responseBuilder = request
+				.evaluatePreconditions(lastUpdatedAt, tag);
 
-            Response.ResponseBuilder responseBuilder = request.evaluatePreconditions(lastUpdated,tag);
-
-            if (responseBuilder != null) {
-                responseBuilder.lastModified(section.getUpdatedAt());
-                return responseBuilder.build();
-            }
-            responseBuilder = Response.ok(resource);
-            responseBuilder.lastModified(section.getUpdatedAt());
-            responseBuilder.tag(tag);
-            return responseBuilder.build();
-
+		if (responseBuilder != null) {
+			responseBuilder.lastModified(section.getUpdatedAt());
+			return responseBuilder.build();
 		}
-		return Response.ok(Response.Status.NOT_FOUND).build();
+		responseBuilder = Response.ok(resources);
+		responseBuilder.lastModified(section.getUpdatedAt());
+		responseBuilder.tag(tag);
+		return responseBuilder.build();
 	}
 
 	/**
 	 * Find Section By Section's Course Name
 	 *
-	 * @param name name of the course
-	 * @return Response.Status.OK with Details of Sections which match the course name
+	 * @param name
+	 *            name of the course
+	 * @return Response.Status.OK with Details of Sections which match the
+	 *         course name
 	 */
 	@GET
 	@Path("/coursename:{name}")
@@ -103,62 +117,63 @@ public class SectionResource {
 	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response findSections(@QueryParam("page") @DefaultValue("1") int page,
-                                 @QueryParam("size") @DefaultValue("2") int size,
-                                 @Context UriInfo uriInfo) {
-        if(page<1 || size<1){
-            return Response.status(400).build();
-        }
+	public Response findSections(
+			@QueryParam("page") @DefaultValue("1") int page,
+			@QueryParam("size") @DefaultValue("2") int size,
+			@Context UriInfo uriInfo) {
+		if (page < 1 || size < 1) {
+			return Response.status(400).build();
+		}
 
-        List<Section> allSections = sectionService.findAllSections();
+		List<Section> allSections = sectionService.findAllSections();
 
 		SectionAssembler sectionAssembler = new SectionAssembler();
 		List<SectionResourceWrapper> resources = sectionAssembler
 				.toResources(allSections);
 
+		List<SectionResourceWrapper> toShow = Lists.newArrayList();
+		for (int i = (page - 1) * size, j = 0; j < size && i < resources.size(); i++, j++) {
+			toShow.add(resources.get(i));
+		}
 
-        List<SectionResourceWrapper> toShow = Lists.newArrayList();
-        for(int i=(page-1)*size, j=0;j<size && i<resources.size(); i++,j++){
-            toShow.add(resources.get(i));
-        }
+		int totalNumberOfPages = resources.size() / size;
+		totalNumberOfPages = resources.size() % size != 0 ? totalNumberOfPages + 1
+				: totalNumberOfPages;
 
-        int totalNumberOfPages = resources.size() / size;
-        totalNumberOfPages = resources.size()%size != 0?totalNumberOfPages+1:totalNumberOfPages;
+		List<Link> links = PaginationHelper.getPaginationLinks(page, size,
+				uriInfo, totalNumberOfPages);
 
-        List<Link> links = PaginationHelper.getPaginationLinks(page, size, uriInfo, totalNumberOfPages);
+		PagedResources<SectionResourceWrapper> sectionResourceWrappers = new PagedResources<>(
+				toShow, new PagedResources.PageMetadata(size, page,
+						resources.size(), (long) totalNumberOfPages), links);
 
-        PagedResources<SectionResourceWrapper> sectionResourceWrappers = new PagedResources<>(
-            toShow,
-            new PagedResources.PageMetadata(
-                size,
-                page,
-                resources.size(),
-                (long) totalNumberOfPages),links
-        );
-
-		return Response.ok(sectionResourceWrappers).cacheControl(getCacheControl()).build();
+		return Response.ok(sectionResourceWrappers)
+				.cacheControl(getCacheControl()).build();
 	}
 
 	/**
 	 * Creates a Section
 	 *
-	 * @param section data of the new section to be created
+	 * @param section
+	 *            data of the new section to be created
 	 * @return
 	 * @throws ApplicationException
 	 */
 	@POST
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_JSON)
-	@RolesAllowed({"PROFESSOR","ADMIN"})
-	public Response addSection(Section section,@Context UriInfo uriInfo) throws ApplicationException {
+	@RolesAllowed({ "PROFESSOR", "ADMIN" })
+	public Response addSection(Section section, @Context UriInfo uriInfo)
+			throws ApplicationException {
 
 		boolean isSaved = sectionService.addSection(section);
 		if (isSaved) {
 			String result = "Section saved : " + section;
-            String id = section.getId()!=null?section.getId().toString():"";
-			return Response.created(uriInfo.getAbsolutePathBuilder().path(id).build())
-                .entity(result)
-                .build();
+			String id = section.getId() != null ? section.getId().toString()
+					: "";
+			return Response
+					.created(uriInfo.getAbsolutePathBuilder().path(id).build())
+					.entity(result).build();
 		} else {
 			String result = "Section is not saved" + section;
 			return Response.status(400).entity(result).build();
@@ -174,7 +189,7 @@ public class SectionResource {
 	@DELETE
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-    @RolesAllowed({"PROFESSOR","ADMIN"})
+	@RolesAllowed({ "PROFESSOR", "ADMIN" })
 	public Response deleteSection(@PathParam("id") Long section_id)
 			throws ApplicationException {
 		sectionService.deleteSection(section_id);
@@ -191,7 +206,7 @@ public class SectionResource {
 	@PUT
 	@Path("/{section_id}/price/update/{price}")
 	@Produces(MediaType.APPLICATION_JSON)
-    @RolesAllowed({"PROFESSOR","ADMIN"})
+	@RolesAllowed({ "PROFESSOR", "ADMIN" })
 	public Response updateSectionPrice(@PathParam("section_id") Long id,
 			@PathParam("price") int price) {
 		boolean isUpdated = sectionService.updateSectionPrice(id, price);
@@ -213,9 +228,9 @@ public class SectionResource {
 	@PUT
 	@Path("{sectionId}")
 	@Produces(MediaType.TEXT_PLAIN)
-    @RolesAllowed({"PROFESSOR","ADMIN"})
-	public Response updateSection(@PathParam("sectionId") long id, Section current)
-			throws ApplicationException {
+	@RolesAllowed({ "PROFESSOR", "ADMIN" })
+	public Response updateSection(@PathParam("sectionId") long id,
+			Section current) throws ApplicationException {
 		boolean isUpdated = sectionService.updateSection(id, current);
 		if (isUpdated)
 			return Response.ok(Response.Status.OK)
@@ -310,53 +325,52 @@ public class SectionResource {
 			@QueryParam("dayofweek") String dayofweek,
 			@QueryParam("semester") String semester,
 			@QueryParam("coursecode") String coursecode,
-            @Context UriInfo uriInfo)
-			throws ApplicationException {
+			@Context UriInfo uriInfo) throws ApplicationException {
 		Map<SearchCriteria, String> criteria = new HashMap<SearchCriteria, String>();
 		String priceStr = "" + price;
 		String gtepriceStr = "" + gteprice;
 		String ltepriceStr = "" + lteprice;
 
-        Map<String,Object> queryParams = Maps.newHashMap();
+		Map<String, Object> queryParams = Maps.newHashMap();
 
 		if (semester != null && !semester.isEmpty()) {
 			criteria.put(SearchCriteria.SEMESTER_EQUALS, semester);
-            queryParams.put("semester",semester);
+			queryParams.put("semester", semester);
 		}
 		if (dayofweek != null && !dayofweek.isEmpty()) {
 			criteria.put(SearchCriteria.DAY_OF_WEEK_EQUALS, dayofweek);
-            queryParams.put("dayofweek",dayofweek);
+			queryParams.put("dayofweek", dayofweek);
 		}
 		if (lastname != null && !lastname.isEmpty()) {
 			criteria.put(SearchCriteria.PROFESSOR_LAST_NAME_CONTAINS, lastname);
-            queryParams.put("lastname",lastname);
+			queryParams.put("lastname", lastname);
 		}
 		if (coursename != null && !coursename.isEmpty()) {
 			criteria.put(SearchCriteria.COURSE_NAME_CONTAINS, coursename);
-            queryParams.put("coursename",coursename);
+			queryParams.put("coursename", coursename);
 		}
 		if (coursecode != null && !coursecode.isEmpty()) {
 			criteria.put(SearchCriteria.COURSE_CODE_EQUALS, coursecode);
-            queryParams.put("coursecode",coursecode);
+			queryParams.put("coursecode", coursecode);
 		}
 		if (!priceStr.trim().isEmpty() && !priceStr.equalsIgnoreCase("null")
-            &&!priceStr.equals("0")) {
+				&& !priceStr.equals("0")) {
 			criteria.put(SearchCriteria.SECTION_PRICE_EQUALS, "" + price);
-            queryParams.put("price",priceStr);
+			queryParams.put("price", priceStr);
 		}
 		if (!gtepriceStr.trim().isEmpty()
 				&& !gtepriceStr.equalsIgnoreCase("null")
-                && !gtepriceStr.equals("0")) {
+				&& !gtepriceStr.equals("0")) {
 			criteria.put(SearchCriteria.SECTION_PRICE_GREATER_EQUALS, ""
 					+ gtepriceStr);
-            queryParams.put("gteprice",gtepriceStr);
+			queryParams.put("gteprice", gtepriceStr);
 		}
 		if (!ltepriceStr.trim().isEmpty()
 				&& !ltepriceStr.equalsIgnoreCase("null")
-                && !ltepriceStr.equals("0")) {
+				&& !ltepriceStr.equals("0")) {
 			criteria.put(SearchCriteria.SECTION_PRICE_LESS_EQUALS, ""
 					+ ltepriceStr);
-            queryParams.put("lteprice",ltepriceStr);
+			queryParams.put("lteprice", ltepriceStr);
 		}
 		if (criteria.size() > 0) {
 
@@ -369,13 +383,13 @@ public class SectionResource {
 			Resources<SectionResourceWrapper> wrapped = new Resources<>(
 					resources);
 
-            UriBuilder linkBuilder = uriInfo.getAbsolutePathBuilder();
-            for(Map.Entry<String,Object> entry:queryParams.entrySet()){
-                linkBuilder.queryParam(entry.getKey(),entry.getValue());
-            }
+			UriBuilder linkBuilder = uriInfo.getAbsolutePathBuilder();
+			for (Map.Entry<String, Object> entry : queryParams.entrySet()) {
+				linkBuilder.queryParam(entry.getKey(), entry.getValue());
+			}
 
-            Link link = new Link(linkBuilder.build().toString(),Link.REL_NEXT);
-            wrapped.add(link);
+			Link link = new Link(linkBuilder.build().toString(), Link.REL_NEXT);
+			wrapped.add(link);
 
 			return Response.ok(wrapped).build();
 		}
