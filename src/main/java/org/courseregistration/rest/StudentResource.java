7ca98c088a4bb22762d9175823ec5411adf98c76
Neha-Ramachandra <nralka90@gmail.com>
2015-11-24 08:49:08 -0800

    updated dropSection method

--- src/main/java/org/courseregistration/rest/StudentResource.java
+++ src/main/java/org/courseregistration/rest/StudentResource.java
@@ -41,79 +41,86 @@ import static org.courseregistration.rest.ResponseHelper.getCacheControl;
 @Path("students")
 @PermitAll
 public class StudentResource {
-    @Autowired
-    private StudentService studentService;
-    @Autowired
-    private StudentDAO studentDAO;
-    @Autowired
-    private SectionDAO sectionDAO;
-    @Autowired
-    private EntityLinks entityLinks;
+	@Autowired
+	private StudentService studentService;
+	@Autowired
+	private StudentDAO studentDAO;
+	@Autowired
+	private SectionDAO sectionDAO;
+	@Autowired
+	private EntityLinks entityLinks;
 
-    /**
-     * Get details of a specific student
-     *
-     * @response.representation.200.doc Details of student
-     * @response.representation.200.mediaType application/json
-     *
-     * @response.representation.404.doc Requested student with id not found
-     *
-     * @return details of a student
-     */
+	/**
+	 * Get details of a specific student
+	 * 
+	 * @response.representation.200.doc Details of student
+	 * @response.representation.200.mediaType application/json
+	 * 
+	 * @response.representation.404.doc Requested student with id not found
+	 * 
+	 * @return details of a student
+	 */
 
-    // add single student
-    @POST
-    @Produces(MediaType.TEXT_PLAIN)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @PermitAll
-    public Response addStudent(@Context UriInfo uriInfo,Student student) throws ApplicationException {
-        studentService.addStudent(student);
-        return Response.created(uriInfo.getAbsolutePathBuilder()
-            .path(student.getId().toString()).build())
-            .entity("Student successfully created")
-            .build();
-    }
+	// add single student
+	@POST
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@PermitAll
+	public Response addStudent(@Context UriInfo uriInfo, Student student)
+			throws ApplicationException {
+		studentService.addStudent(student);
+		return Response
+				.created(
+						uriInfo.getAbsolutePathBuilder()
+								.path(student.getId().toString()).build())
+				.entity("Student successfully created").build();
+	}
 
-    // add multiple students
-    @POST
-    @Path("list")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @RolesAllowed({"PROFESSOR","ADMIN"})
-    public Response addStudents(List<Student> students) throws ApplicationException {
-        studentService.addStudents(students);
-        return Response.ok(200).entity(students).build();
-    }
+	// add multiple students
+	@POST
+	@Path("list")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@RolesAllowed({ "PROFESSOR", "ADMIN" })
+	public Response addStudents(List<Student> students)
+			throws ApplicationException {
+		studentService.addStudents(students);
+		return Response.ok(200).entity(students).build();
+	}
 
-    //delete single student
-    @DELETE
-    @Path("{id}")
-    @Produces(MediaType.TEXT_PLAIN)
-    @RolesAllowed({"PROFESSOR","ADMIN"})
-    public Response deleteStudent(@PathParam("id") Long studentId) throws ApplicationException {
-        studentService.deleteStudent(studentId);
-        return Response.ok().entity("Successfully deleted student with id :"+ studentId).build();
-    }
+	// delete single student
+	@DELETE
+	@Path("{id}")
+	@Produces(MediaType.TEXT_PLAIN)
+	@RolesAllowed({ "PROFESSOR", "ADMIN" })
+	public Response deleteStudent(@PathParam("id") Long studentId)
+			throws ApplicationException {
+		studentService.deleteStudent(studentId);
+		return Response.ok()
+				.entity("Successfully deleted student with id :" + studentId)
+				.build();
+	}
 
-    //delete list of students
-    @DELETE
-    @Path("list/{ids}")
-    @Produces(MediaType.TEXT_PLAIN)
-    @RolesAllowed({"PROFESSOR","ADMIN"})
-    public Response deleteAllStudents(@PathParam("ids") String ids) throws ApplicationException {
-        String [] splitIds = ids.split(",");
-        List<Long> toDelete = newArrayList();
+	// delete list of students
+	@DELETE
+	@Path("list/{ids}")
+	@Produces(MediaType.TEXT_PLAIN)
+	@RolesAllowed({ "PROFESSOR", "ADMIN" })
+	public Response deleteAllStudents(@PathParam("ids") String ids)
+			throws ApplicationException {
+		String[] splitIds = ids.split(",");
+		List<Long> toDelete = newArrayList();
 
-        for(String str:splitIds){
-            toDelete.add(new Long(str));
-        }
-        studentService.deleteAllStudents(toDelete);
-        return Response.ok().entity("Deleted Students" + ids).build();
-    }
+		for (String str : splitIds) {
+			toDelete.add(new Long(str));
+		}
+		studentService.deleteAllStudents(toDelete);
+		return Response.ok().entity("Deleted Students" + ids).build();
+	}
 
-    //get all students
-    //@GET
-    @Produces(MediaType.APPLICATION_JSON)
+	// get all students
+	// @GET
+	@Produces(MediaType.APPLICATION_JSON)
     public Response findStudents() throws ApplicationException {
         List<Student> allStudents = studentService.findAllStudents();
 
@@ -127,31 +134,7 @@ public class StudentResource {
         return Response.ok(wrapped).build();
     }
 
-    //@GET
-    //@Path("{id}")
-    //@Produces(MediaType.APPLICATION_JSON)
-    public Response viewSectionsListOfAStudent(@PathParam("id") Long id, @PathParam("section_id") Long section_id,
-                                          @Context Request request) throws ApplicationException {
-        Student fromDB = studentDAO.findById(id);
-        List<Section> section = fromDB.getSections();
-        if (section != null) {
-
-            SectionAssembler sectionAssembler = new SectionAssembler();
-            List<SectionResourceWrapper> resources = sectionAssembler.toResources(section);
-
-            Resources<SectionResourceWrapper> wrapped = new Resources<>(resources);
-
-            Link selfRel = entityLinks
-                .linkToSingleResource(StudentResource.class, dropSection(id, section_id))
-                .withSelfRel();
-            wrapped.add(selfRel);
-            return Response.ok(200).entity("Enrolled to these Sections").build();
-        }
-        return Response.ok(Response.Status.NOT_FOUND).build();
-    }
-
-
-    @GET
+	@GET
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
     public Response findStudentsPaged(@QueryParam("page") @DefaultValue("1") int page,
@@ -188,71 +171,97 @@ public class StudentResource {
         return Response.ok(studentResourceWrappers).cacheControl(getCacheControl()).build();
     }
 
-    //get student by id
-    @GET
-    @Path("{id}")
-    @CacheMaxAge(time = 10, unit = TimeUnit.MINUTES, isPrivate=false,noStore=false)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response findStudentById(@Context UriInfo uriInfo,@PathParam("id") Long id,
-                                    @Context Request request) throws ApplicationException {
-        Student student = studentService.findById(id);
-        StudentAssembler studentAssembler = new StudentAssembler();
-        StudentResourceWrapper resources = studentAssembler.toResource(student);
+	// get student by id
+	@GET
+	@Path("{id}")
+	@CacheMaxAge(time = 10, unit = TimeUnit.MINUTES, isPrivate = false, noStore = false)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response findStudentById(@Context UriInfo uriInfo,
+			@PathParam("id") Long id, @Context Request request)
+			throws ApplicationException {
+		Student student = studentService.findById(id);
+		StudentAssembler studentAssembler = new StudentAssembler();
+		StudentResourceWrapper resources = studentAssembler.toResource(student);
 
+		int hashCode = student.hashCode();
 
-        int hashCode = student.hashCode();
+		EntityTag tag = new EntityTag(Integer.toString(hashCode));
+		Date lastUpdatedAt = student.getUpdatedAt();
 
-        EntityTag tag = new EntityTag(Integer.toString(hashCode));
-        Date lastUpdatedAt = student.getUpdatedAt();
+		Response.ResponseBuilder responseBuilder = request
+				.evaluatePreconditions(lastUpdatedAt, tag);
 
-        Response.ResponseBuilder responseBuilder = request.evaluatePreconditions(lastUpdatedAt,tag);
+		if (responseBuilder != null) {
+			responseBuilder.lastModified(student.getUpdatedAt());
+			return responseBuilder.build();
+		}
+		responseBuilder = Response.ok(resources);
+		responseBuilder.lastModified(student.getUpdatedAt());
+		responseBuilder.tag(tag);
+		return responseBuilder.build();
+	}
 
-        if (responseBuilder != null) {
-            responseBuilder.lastModified(student.getUpdatedAt());
-            return responseBuilder.build();
-        }
-        responseBuilder = Response.ok(resources);
-        responseBuilder.lastModified(student.getUpdatedAt());
-        responseBuilder.tag(tag);
-        return responseBuilder.build();
-    }
+	// update student details
+	@PUT
+	@Path("{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response updateStudent(@PathParam("id") Long id, Student s,
+			@Context Request request) throws ApplicationException {
+		Student fromDB = studentDAO.findById(id);
 
-    //update student details
-    @PUT
-    @Path("{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response updateStudent(@PathParam("id")Long id, Student s,
-                                  @Context Request request) throws ApplicationException {
-        Student fromDB = studentDAO.findById(id);
+		EntityTag tag = new EntityTag(Integer.toString(fromDB.hashCode()));
+		Date timestamp = fromDB.getUpdatedAt();
 
-        EntityTag tag = new EntityTag(Integer.toString(fromDB.hashCode()));
-        Date timestamp = fromDB.getUpdatedAt();
+		Response.ResponseBuilder builder = request.evaluatePreconditions(
+				timestamp, tag);
+		if (builder != null) {
+			return builder.build();
+		}
+		Student stud = studentService.updateStudent(id, fromDB, s);
+		return Response
+				.noContent()
+				.entity("Successfully updated Student with id :" + stud.getId())
+				.build();
+	}
 
-        Response.ResponseBuilder builder =request.evaluatePreconditions(timestamp, tag);
-        if (builder != null) {
-            return builder.build();
-        }
-        Student stud = studentService.updateStudent(id,fromDB, s);
-        return Response.noContent().entity("Successfully updated Student with id :"+stud.getId()).build();
-    }
+	// enroll to a section
+	@POST
+	@Path("{id}/sections/{section_id}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response enrollSection(@PathParam("id") Long id,
+			@PathParam("section_id") Long section_id)
+			throws ApplicationException {
+		String status = studentService.enrollSection(id, section_id);
+		return Response.ok(200).entity(status).build();
+	}
 
-    //enroll to a section
-    @POST
-    @Path("{id}/sections/{section_id}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response enrollSection(@PathParam("id") Long id,@PathParam("section_id") Long section_id) throws ApplicationException {
-        String status = studentService.enrollSection(id, section_id);
-        return Response.ok(200).entity(status).build();
-    }
+	@DELETE
+	@Path("{id}/sections/{section_id}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response dropSection(@PathParam("id") Long id,
+			@PathParam("section_id") Long section_id, @Context Request request)
+			throws ApplicationException {
+		Section fromDB = sectionDAO.findById(section_id);
+		Date sectionStartDate = fromDB.getStartDate();
+		Date today = new Date();
 
-    //delete a single section
-    @DELETE
-    @Path("{id}/sections/{section_id}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response dropSection(@PathParam("id") Long id, @PathParam("section_id")Long section_id) throws ApplicationException {
-        String status = studentService.dropSection(id, section_id);
-        return Response.ok(200).entity(status).build();
-    }
+		int diffInDays = (int) ((today.getTime() - sectionStartDate.getTime()) / (1000 * 60 * 60 * 24));
+
+		if (diffInDays <= 10) {
+			studentService.dropSection(id, section_id);
+			SectionAssembler sectionAssembler = new SectionAssembler();
+			SectionResourceWrapper resources = sectionAssembler
+					.toResource(fromDB);
+
+			resources.add(JaxRsLinkBuilder.linkTo(StudentResource.class)
+					.withSelfRel());
+
+			return Response.ok(200).entity("Dropped Section").build();
+		} else {
+			return Response.ok(405)
+					.entity("Past due date. Deletion is not possible").build();
+		}
+	}
 
 }
